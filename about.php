ba196c11f1ad7d5257d8553b11db84d9500769c8
105730438 <105730438@student.swin.edu.au>
2025-05-16 14:55:34 +1000

    files added

---
+++ about.php
@@ -0,0 +1,102 @@
+<?php include("header.inc"); ?>
+<?php include("menu.inc"); ?>
+
+
+<main class="main">
+        <div class="top-part">
+            <div class="product-purpose2">
+                <h1>About.</h1>
+            </div>
+            <img id="top-image" src="images/pinktree.png" alt="tree image">
+            <div class="product-purpose3">
+                <h1>Invent what's next—responsibly.</h1>
+                <p class="after-purpose-text1">We're building AI that elevates people and society, and we're looking for creative, mission-driven individuals to help lead the way.</p>
+                <p class="after-purpose-text2">Values: These values reflect what we stand for. They guide our decisions, shape our culture, and help us build AI with purpose.</p>
+                <p class="after-purpose-text3">Bold, Responsible Innovation. We move fas...</p>
+                <div class="see-more">See more</div>
+            </div>
+        </div>
+        <div>
+            <div class="mid-part">
+                <div class="title-wrapper">
+                    <h1>Our Team</h1>
+                </div>
+                <div class="our-team-top-wrapper">
+                    <figure>
+                        <img class="rounded-image" src="images/groupimage.png" alt="Group Photo" width="300">
+                    </figure>
+                    <p>Greeting. We are Earthling. Below are information regarding our group</p>
+                </div>
+                <table class="general-info-table">
+                    <tr>
+                        <th>Group Information</th>
+                        <th>Student IDs</th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <ul>
+                                <li>Group Name: Earthlings</li>
+                                <li>Class Time: 14:30 - 16:30</li>
+                                <li>Day: Friday</li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li>Hien Long Dang: 105730438</li>
+                                <li>Quoc Tri Nguyen: 105544639</li>
+                                <li>Quang Dung Nguyen: 103810008</li>
+                            </ul>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th colspan="2">Members Contributions</th>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <li>Hien Long Dang: Tasks 1,2,6,8</li>
+                                <li>Quoc Tri Nguyen: Tasks 5,7</li>
+                                <li>Quang Dung Nguyen: Tasks 3,4</li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+        <div class="about-last-part">
+            <table class="interests-table">
+                <caption><h2><strong>Group Member Interests</strong></h2></caption>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Interests</th>
+                        <th>Programming Skills</th>
+                        <th>From</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Hien Long Dang</td>
+                        <td>Video games, work</td>
+                        <td>Barely enough</td>
+                        <td>Vietnam</td>
+                    </tr>
+                    <tr>
+                        <td>Quoc Tri Nguyen</td>
+                        <td>Listen to music, going out, work</td>
+                        <td>Barely enough</td>
+                        <td>Vietnam</td>
+                    </tr>
+                    <tr>
+                        <td>Quang Dung Nguyen</td>
+                        <td>Listen to music, reading books</td>
+                        <td>Barely enough</td>
+                        <td>Vietnam</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+
+    <?php include("footer.inc"); ?>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php include("header.inc"); ?>
+<?php include("menu.inc"); ?>
+
+<main class="main">
+        <div class="top-part">
+            <img id="top-image" src="images/topportion.png" alt="sky image">
+            <div class="product-purpose">
+                <h1>The AI Tool To Expand Your Creativity</h1>
+                <p>Design to improve you productivity</p>
+            </div>
+        </div>
+        <div class="overview">
+            <div>
+                <p class="image-title">It's a <span id="partner">partner</span> who always stands by your side on creative projects, from initial brainstorming to final editing.</p>
+                <img class="overview-images" src="images/storyprompt.png" alt="Story Prompt">
+            </div>
+            <div>
+                <p class="image-title">A <span id="tutor">tutor</span> who helps you turn your own ideas into working code.</p>
+                <img class="overview-images" src="images/codeprompt.png" alt="Code Prompt">
+            </div>
+            <div>
+                <p class="image-title">Whether you have a simple idea or a detailed description, it can create an image to match your needs.</p>
+                <img class="overview-images" src="images/imageprompt.png" alt="Image Prompt">
+            </div>
+            <div class="see-more">Explore more features of Earthling AI</div>
+        </div>
+        <div class="mid-part">
+            <div class="research">
+                <h2>Latest research</h2>
+            </div>
+            <div class="news">
+                <div class="left-news">
+                    <img id="research-left" src="images/bluepallete.png" alt="Left News Image">
+                    <div class="image-label">Introducing the XXXXX benchmark</div>
+                    <div class="image-date">Jan 24, 2025</div>
+                </div>
+                <div class="right-news">
+                    <img id="research-right" src="images/flower.png" alt="Right News Image">
+                    <div class="image-label">Introducing the YYYYY model</div>
+                    <div class="image-date">Mar 11, 2025</div>
+                </div>
+            </div>
+            <div class="see-more">See more</div>
+        </div>
+        <div class="bottom-part">
+            <h1 class="getting-started">Getting Started</h1>
+            <div class="download-button">DOWNLOAD</div>
+        </div>
+    </main>
+
+    <?php include("footer.inc"); ?>
